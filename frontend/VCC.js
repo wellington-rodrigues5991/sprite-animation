6c210b91a646147952343955c0c9a80e120e68aa
WellingtonRodrigues4 <WellingtonRodrigues4@koji-projects.com>
2019-12-20 22:19:19 +0000

    Manual deploy

--- frontend/VCC.js
+++ frontend/VCC.js
@@ -94,6 +94,8 @@ class App extends React.PureComponent {
             state.value.scale = (value.scale == undefined ? state.scale : value.scale);
             state.value.jump = (value.jump == undefined ? state.jump : value.jump);
 
+            console.log(state.value.scale)
+
             this.setState({state});
         });
 
